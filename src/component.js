796511bb25f1c37dfdab44e846069b4f3e09e676
Dmitry Basov <dmbasov@gmail.com>
2021-06-07 14:45:07 +0300

    switch to reactive

--- src/component.js
+++ src/component.js
@@ -1,19 +1,308 @@
 import XLSX from "xlsx";
 import { saveAs } from "file-saver";
 
-const {
-  Component,
-  utils,
-  iconset,
-} = Vizabi;
+import {
+  BaseComponent,
+  LegacyUtils as utils,
+  Utils
+} from "VizabiSharedComponents";
+
+import {
+  decorate,
+  computed
+} from "mobx";
+
 
 const charts = {
-  BubbleChart: {toolsPageChartType: "bubbles", icon: "🎈", hook: "axis_y"},
-//  BubbleMap: {toolsPageChartType: "map", icon: "🌍", hook: "color"},
-  LineChart: {toolsPageChartType: "linechart", icon: "〽️", hook: "axis_y"}
+  BubbleChart: {toolsPageChartType: "bubbles", marker: "bubble", icon: "🎈", encoding: "y"},
+//  BubbleMap: {toolsPageChartType: "map", marker: "bubble", icon: "🌍", encoding: "color"},
+  LineChart: {toolsPageChartType: "linechart", marker: "line", icon: "〽️", encoding: "y"}
 };
 
-const Spreadsheet = Component.extend("spreadsheet", {
+class _VizabiSpreadsheet extends BaseComponent {
+  constructor (config) {
+
+      config.template = `
+        <div id="vzb-spreadsheet-title"></div>
+        <div id="vzb-spreadsheet-about"></div>
+        <div id="vzb-spreadsheet-actions"></div>
+        <div id="vzb-spreadsheet-table"></div>  
+      `;
+
+    super(config);
+  }
+
+  setup() {
+    this.DOM = {
+      title: this.element.select("#vzb-spreadsheet-title"),
+      about: this.element.select("#vzb-spreadsheet-about"),
+      actions: this.element.select("#vzb-spreadsheet-actions"),
+      table: this.element.select("#vzb-spreadsheet-table")
+    }
+
+    this.root.element.classed("vzb-timeslider-off", true);
+
+    //this.KEYS = utils.unique(this.model.marker._getAllDimensions({ exceptType: "time" }));
+    //this.KEY = this.KEYS.join(",");
+
+
+    this.treemenu = () => this.root.findChild({type: "TreeMenu"});
+  }
+
+  draw() {
+    this.localise = this.services.locale.auto();
+    this.fixHeaders = this.ui.fixHeaders;
+
+    //if (this.updateLayoutProfile()) return; //return if exists with error
+
+    this.treemenu()
+      .alignX(this.services.locale.isRTL() ? "right" : "left")
+      .alignY("top")
+      .title("")
+      .scaletypeSelectorDisabled(true)
+      .encoding("hook");
+
+    if (!this.model.encoding.hook.data.concept) {
+      this.treemenu().showWhenReady(true);
+      if (this.treemenu().state.ownReadiness !== Utils.STATUS.READY) return;
+    }
+
+    this.addReaction(this._drawTitle);
+    this.addReaction(this._drawAboutSection);
+    this.addReaction(this._drawActionsSection);
+    this.addReaction(this._drawDataTable);  
+  }
+
+  _drawTitle() {
+    const concept = this.model.encoding.hook.data.conceptProps;
+  
+    this.DOM.title
+      .classed("vzb-disabled", this.treemenu().state.ownReadiness !== Utils.STATUS.READY)
+      .on("click", () => this.treemenu().updateView().toggle())
+      .text(concept?.name);
+  }
+
+  get dataMap() {
+    return this.model.dataMapCache.groupBy(this.model.dataMapCache.key.slice(0, -1), this.model.dataMapCache.key.slice(-1));
+  }
+
+  _drawDataTable() {
+    const concept = this.model.encoding.hook.data.conceptProps;
+    if (!concept) return;
+
+    const _this = this;
+
+    this.DOM.table.select(".viz-spreadsheet-keytable-wrapper").remove();
+    this.DOM.table.select(".viz-spreadsheet-table-wrapper").remove();
+    this.DOM.table.classed("vzb-spreadsheet-table-fix-headers", this.fixHeaders);
+    this.DOM.table.append("div").attr("class","vzb-spreadsheet-loading").text("data table is loading...");
+    
+    //const KEYS = this.KEYS;
+    //const dataKeys =  this.dataKeys = this.model.marker.getDataKeysPerHook();
+    //const labelNames = this.labelNames = this.model.marker.getLabelHookNames();
+
+    const frameConcept = this.model.encoding.frame.data.concept;
+    const steps = this.model.encoding.frame.domainValues.map(v => ({[frameConcept]: v}));
+    const timeFormatter = this.localise;
+    const valueFormatter = this.localise;
+    const KEYS = this.dataMap.key;
+
+    // const keys = this.model.marker.getKeys()
+    //   .map(key => { 
+    //     key[Symbol.for('key')] = utils.getKey(key, dataKeys.hook);
+    //     return key;
+    //   })
+    //   .filter(key => steps.some(step => values[step].hook[key[Symbol.for('key')]] || values[step].hook[key[Symbol.for('key')]] === 0))
+    //   .sort(function(b, a) {
+    //     return d3.descending(values[_this.model.time.value].label[utils.getKey(a, dataKeys.label)], values[_this.model.time.value].label[utils.getKey(b, dataKeys.label)]);
+    //   });
+
+    this.DOM.table.select("div.vzb-spreadsheet-loading").remove();
+    this.DOM.actions.classed("vzb-hidden", false);
+
+    const table = this.DOM.table
+    .append("div")
+      .classed("viz-spreadsheet-table-wrapper", true)
+    .append("table")
+      .attr("id", "table_" + this._id)
+      .classed("viz-spreadsheet-table", true)
+
+    table.selectAll("tr").data([{}, ...this.dataMap.values()])
+      .enter().append("tr")
+      .attr("class", (d, i) => i ? "viz-spreadsheet-tablerow" : "viz-spreadsheet-headrow")
+      .each(function(r, i){
+        const labelObj = i == 0 ? {} : r.values().next().value.label;
+        const row = d3.select(this).selectAll("td").data(KEYS.concat(steps))
+          .enter().append("td")
+          .classed("viz-spreadsheet-keycell", (c,j) => j<KEYS.length)
+          .text((c, j) => {
+            if (i==0 && j<KEYS.length) return c;
+            if (j<KEYS.length) return labelObj[c];
+            if (i==0) return timeFormatter(c[frameConcept]);
+            return valueFormatter(r.get(c)?.hook) || "";
+          })
+      });
+
+    if (this.fixHeaders) {
+
+      const keysTable = this.DOM.table
+        .append("div")
+          .classed("viz-spreadsheet-keytable-wrapper", true)
+        .lower()
+        .append("table")
+          .attr("id", "table_keys_" + this._id)
+          .classed("viz-spreadsheet-keytable", true)
+
+      keysTable.selectAll("tr").data([{}, ...this.dataMap.values()])
+        .enter().append("tr")
+        .attr("class", (d, i) => i ? "viz-spreadsheet-tablerow" : "viz-spreadsheet-headrow")
+        .each(function(r, i){
+          const labelObj = i == 0 ? {} : r.values().next().value.label;
+          const row = d3.select(this).selectAll("td").data(KEYS)
+            .enter().append("td")
+            .attr("data-caption", c => i == 0 ? c : null)
+            .text((c, j) => {
+              if (i==0) return "";
+              return labelObj[c];
+            })
+        });
+
+      const tableHeader = table.clone(true).lower()
+        .attr("id", "table_header_" + _this._id)
+        .attr("class", "viz-spreadsheet-table-header");
+
+      const scrollBarWidth = table.node().offsetWidth - table.node().clientWidth;
+      tableHeader.style("margin-right", scrollBarWidth + "px");
+      keysTable.style("height", `calc(100% - ${scrollBarWidth}px)`);
+
+      table.on("scroll", function () {
+        keysTable.node().scrollTop = this.scrollTop;
+        tableHeader.node().scrollLeft = this.scrollLeft;
+      });
+    }
+  }
+
+  _drawAboutSection() {
+    const concept = this.model.encoding.hook.data.conceptProps;
+    if (!concept) return;
+
+    const _this = this;
+
+//    const dataAvailable = this.model.data.dataAvailability.datapoints
+//      .reduce((result, item) => {
+//        if (item.value !== which) return result;
+//        result.push({
+//          key: [...item.key].join(","),
+//          value: [...item.key]
+//        })
+//        return result;
+//      }, []);
+//
+//    let viewAbout = this.element.select("#vzb-spreadsheet-content").select("#about");
+//
+//    const selectorEl = viewAbout.append("div").style("display", dataAvailable.length > 1 ? "block" : "none")
+//      .call(selection => selection.append("div").classed("vzb-spreadsheet-conceptkey", true).text("dimensions"))
+//      .append("div").classed("vzb-spreadsheet-conceptvalue", true)
+//      .append("div").classed("vzb-spreadsheet-select", true)
+//      .append("select").on("change", function(evt) {
+//        const d = d3.select(this.options[this.options.selectedIndex]).datum();
+//        _this.model.marker.setSpace(d.value);
+//        utils.defer(() => _this.model.marker.startLoading());
+//      })
+//    selectorEl.selectAll("option").data(dataAvailable, d => d.key).enter().append("option").text(d => d.key);
+//
+//    const allKeys = utils.unique(this.model.marker._getAllDimensions()).join(",");
+//    const selectedIndex = dataAvailable.map(d => d.key).indexOf(allKeys);
+//    selectorEl.property("selectedIndex", selectedIndex === -1 ? 0: selectedIndex);
+//    if (selectedIndex === -1) selectorEl.dispatch("change");
+//
+//    this.model.marker.hook.setWhich({concept: which, dataSource: "data"});
+
+    
+    this.DOM.about.selectAll("div").remove();
+    
+    this.DOM.about.selectAll("div.concept")
+      .data(["description", "source", "source_url"].filter(f => concept[f]))
+      .enter().append("div")
+      .html(d=>{
+        let value = d == "source_url" ? utils.normaliseLink(concept[d]) : concept[d];
+        value = (value || "").indexOf("http")==0 ? ('<a href="' + value + '">' + value + '</a>') : value;
+
+        return '<span class="vzb-spreadsheet-conceptkey">' + d + ':</span>' +
+        '<span class="vzb-spreadsheet-conceptvalue">' + value + '</span>'
+      })
+
+
+  }
+
+  _drawActionsSection() {
+    const concept = this.model.encoding.hook.data.conceptProps;
+    if (!concept) return;
+
+    this.DOM.actions.selectAll("div").remove();
+
+    this.DOM.actions.classed("vzb-hidden", true);    
+
+    this.DOM.actions.append("div")
+      .attr("class", "vzb-spreadsheet-viewas")
+      .text("View as:")
+      .selectAll("a").data(Object.keys(charts))
+      .enter().append("a")
+      .text(chart=>charts[chart].icon)
+      .attr("title", chart=>chart)
+      .attr("target", "_blank")
+      .attr("href", chart => this._viewAs(charts[chart], concept)); 
+
+    this.DOM.actions.append("div")
+      .attr("class", "vzb-spreadsheet-downloadas")
+      .text("Download as:")
+      .selectAll("a").data(["csv", "xlsx"])
+      .enter().append("a")
+      .text(type=>type)
+      .on("click", type => this._download(type, concept.concept));  
+  }
+  
+  _viewAs(chart, concept){
+    const scaleType = (JSON.parse(concept.scales) || [])[0] || "linear";
+    return window.location.origin + window.location.pathname 
+      + `#$model$markers$${chart.marker}$encoding$${chart.encoding}$data$concept=${concept.concept};&scale$domain:null&zoomed:null&type=${scaleType};;;;;;&chart-type=${chart.toolsPageChartType}&url=v1`;    
+  }
+
+  _download(type, fileName){
+    function s2ab(s) {
+      if(typeof ArrayBuffer !== 'undefined') {
+        const buf = new ArrayBuffer(s.length);
+        const view = new Uint8Array(buf);
+        for (let i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+      } else {
+        const buf = new Array(s.length);
+        for (let i=0; i!=s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+      }
+    }
+
+    function export_table_to_excel(id, type, fileName) {
+    const wb = XLSX.utils.table_to_book(document.getElementById(id), { sheet: fileName.slice(0, 31) });
+    const wbout = XLSX.write(wb, { bookType: type, bookSST: true, type: 'binary' });
+    const fName = `${fileName}.${type}`;
+    try {
+      saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), fName);
+    } catch(e) { if(typeof console != 'undefined') console.log(e, wbout); }
+    return wbout;
+    }
+
+    export_table_to_excel('table_' + this._id, type, fileName);    
+  }
+
+}
+
+
+
+
+
+
+const _Spreadsheet = {
 
   init(config, context) {
     this.name = "spreadsheet-component";
@@ -53,10 +342,10 @@ const Spreadsheet = Component.extend("spreadsheet", {
   readyOnce() {
     d3.select(this.parent.element).classed("vzb-timeslider-off", true);
     this.element = d3.select(this.element);
-    this.titleEl = this.element.select("#vzb-spreadsheet-title");
-    this.aboutEl = this.element.select("#vzb-spreadsheet-about");
-    this.actionsEl = this.element.select("#vzb-spreadsheet-actions");
-    this.tableEl = this.element.select("#vzb-spreadsheet-table");
+    this.DOM.title = this.element.select("#vzb-spreadsheet-title");
+    this.DOM.about = this.element.select("#vzb-spreadsheet-about");
+    this.DOM.actions = this.element.select("#vzb-spreadsheet-actions");
+    this.DOM.table = this.element.select("#vzb-spreadsheet-table");
 
     this.KEYS = utils.unique(this.model.marker._getAllDimensions({ exceptType: "time" }));
     this.KEY = this.KEYS.join(",");
@@ -71,7 +360,7 @@ const Spreadsheet = Component.extend("spreadsheet", {
       .scaletypeSelectorDisabled(true)
       .markerID("hook");
     
-    this.titleEl.on("click", () => this.treemenu.updateView().toggle());
+    this.DOM.title.on("click", () => this.treemenu.updateView().toggle());
   },
 
   ready() {
@@ -80,7 +369,7 @@ const Spreadsheet = Component.extend("spreadsheet", {
     if(!this.model.marker.hook.which) {
       //open treemenu so that indicator can be selected
       this.treemenu.showWhenReady(true);
-      this.titleEl.text("Select an indicator");
+      this.DOM.title.text("Select an indicator");
     } else {
       const concept = this.model.marker.hook.getConceptprops();
       this._drawTitle(concept);
@@ -94,16 +383,16 @@ const Spreadsheet = Component.extend("spreadsheet", {
   },
   
   _drawTitle(concept) {
-    this.titleEl
+    this.DOM.title
       .text(concept.name);
   },
 
   _drawDataTable(concept) {
     const _this = this;
-    this.tableEl.select(".viz-spreadsheet-keytable-wrapper").remove();
-    this.tableEl.select(".viz-spreadsheet-table-wrapper").remove();
-    this.tableEl.classed("vzb-spreadsheet-table-fix-headers", this.fixHeaders);
-    this.tableEl.append("div").attr("class","vzb-spreadsheet-loading").text("data table is loading...");
+    this.DOM.table.select(".viz-spreadsheet-keytable-wrapper").remove();
+    this.DOM.table.select(".viz-spreadsheet-table-wrapper").remove();
+    this.DOM.table.classed("vzb-spreadsheet-table-fix-headers", this.fixHeaders);
+    this.DOM.table.append("div").attr("class","vzb-spreadsheet-loading").text("data table is loading...");
     
     const KEYS = this.KEYS;
     const dataKeys =  this.dataKeys = this.model.marker.getDataKeysPerHook();
@@ -125,10 +414,10 @@ const Spreadsheet = Component.extend("spreadsheet", {
           return d3.descending(values[_this.model.time.value].label[utils.getKey(a, dataKeys.label)], values[_this.model.time.value].label[utils.getKey(b, dataKeys.label)]);
         });
 
-      _this.tableEl.select("div.vzb-spreadsheet-loading").remove();
-      _this.actionsEl.classed("vzb-hidden", false);
+      _this.table.select("div.vzb-spreadsheet-loading").remove();
+      _this.actions.classed("vzb-hidden", false);
 
-      const table = _this.tableEl
+      const table = _this.table
       .append("div")
         .classed("viz-spreadsheet-table-wrapper", true)
       .append("table")
@@ -151,7 +440,7 @@ const Spreadsheet = Component.extend("spreadsheet", {
         });
 
       if (this.fixHeaders) {
-        const keysTable = _this.tableEl
+        const keysTable = _this.table
           .append("div")
             .classed("viz-spreadsheet-keytable-wrapper", true)
           .lower()
@@ -225,9 +514,9 @@ const Spreadsheet = Component.extend("spreadsheet", {
 //    this.model.marker.hook.setWhich({concept: which, dataSource: "data"});
 
     
-    this.aboutEl.selectAll("div").remove();
+    this.DOM.about.selectAll("div").remove();
     
-    this.aboutEl.selectAll("div.concept")
+    this.DOM.about.selectAll("div.concept")
       .data(["description", "sourceName", "sourceLink"].filter(f => concept[f]))
       .enter().append("div")
       .html(d=>{
@@ -242,11 +531,11 @@ const Spreadsheet = Component.extend("spreadsheet", {
   },
   
   _drawActionsSection(concept){
-    this.actionsEl.selectAll("div").remove();
+    this.DOM.actions.selectAll("div").remove();
     
-    this.actionsEl.classed("vzb-hidden", true);
+    this.DOM.actions.classed("vzb-hidden", true);
     
-    this.actionsEl.append("div")
+    this.DOM.actions.append("div")
       .attr("class", "vzb-spreadsheet-viewas")
       .text("View as:")
       .selectAll("a").data(Object.keys(charts))
@@ -256,7 +545,7 @@ const Spreadsheet = Component.extend("spreadsheet", {
       .attr("target", "_blank")
       .attr("href", chart => this._viewAs(charts[chart], concept)); 
     
-    this.actionsEl.append("div")
+    this.DOM.actions.append("div")
       .attr("class", "vzb-spreadsheet-downloadas")
       .text("Download as:")
       .selectAll("a").data(["csv", "xlsx"])
@@ -300,6 +589,9 @@ const Spreadsheet = Component.extend("spreadsheet", {
   }
 
 
-});
+};
 
-export default Spreadsheet;
+export const VizabiSpreadsheet = decorate(_VizabiSpreadsheet, {
+  "dataMap": computed,
+  //"MDL": computed,
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,83 @@
 import "./styles.scss";
-import "./treemenu-extension.scss";
-import component from "./component";
+import { 
+  BaseComponent,
+  LocaleService,
+  LayoutService,
+  TreeMenu,
+  CapitalVizabiService,
+  versionInfo
+} from "VizabiSharedComponents";
+import { VizabiSpreadsheet } from "./component";
 
 
+export default class Spreadsheet extends BaseComponent {
+
+  constructor(config){
+
+    Vizabi.utils.applyDefaults(config.model.markers.spreadsheet.config, Spreadsheet.DEFAULT_CORE);    
+    const marker = config.model.markers.spreadsheet;
+
+    config.name = "spreadsheet";
+
+    config.subcomponents = [{
+      type: VizabiSpreadsheet,
+      placeholder: ".vzb-spreadsheet",
+      model: marker,
+      name: "chart"
+    },{
+      type: TreeMenu,
+      placeholder: ".vzb-treemenu",
+      model: marker,
+      name: "tree-menu"
+    }];
+
+    config.template = `
+      <div class="vzb-spreadsheet"></div>
+      <div class="vzb-treemenu"></div>
+    `;
+  
+    config.services = {
+      Vizabi: new CapitalVizabiService({Vizabi: config.Vizabi}),
+      locale: new LocaleService(config.locale),
+      layout: new LayoutService({placeholder: config.placeholder})
+    };
+
+    super(config);
+  }
+}
+
+
+Spreadsheet.DEFAULT_UI = {
+  chart: {
+    fixHeaders: true
+  },
+};
+
+Spreadsheet.DEFAULT_CORE = {
+  requiredEncodings: ["hook"],
+  encoding: {
+    "hook": {
+      data: {
+        constant: "true"
+      },
+      scale: {
+        allowedTypes: ["linear", "log", "genericLog", "pow", "point"]
+      }
+    },
+    "label": {
+      data: {
+        modelType: "entityPropertyDataConfig"
+      }
+    },
+    "frame": {
+      modelType: "frame"
+    }
+  }
+}
+
+Spreadsheet.versionInfo = { version: __VERSION, build: __BUILD, package: __PACKAGE_JSON_FIELDS, sharedComponents: versionInfo};
+
+/*
 const treemenuExtension = Vizabi.Component.getCollection().treemenu.extend("treemenu-extension", {
   init(config, context) {
     this._super(config, context);
@@ -10,7 +85,7 @@ const treemenuExtension = Vizabi.Component.getCollection().treemenu.extend("tree
   }
 })
 
-export default Vizabi.Tool.extend("Spreadsheet", {
+Vizabi.Tool.extend("Spreadsheet", {
 
   init(placeholder, externalModel) {
 
@@ -75,3 +150,4 @@ export default Vizabi.Tool.extend("Spreadsheet", {
   }
 
 });
+*/
